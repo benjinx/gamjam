da7f7a5bad12748e40cb214468c2e9693ce441e8
danPC <x2danc@gmail.com>
2020-05-15 19:15:27 -0400

    player

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+   public float speed = 2.0f;
+   private Rigidbody rb;
+
+   private void Start()
+   {
+      rb = GetComponent<Rigidbody>();
+   }
+
+   private void Update()
+   {
+      CheckKeysPressed();
+   }
+
+   private void CheckKeysPressed()
+   {
+      if(Input.GetKeyDown(KeyCode.W))
+      {
+         rb.AddForce(Vector3.forward * speed);
+
+      }
+
+   }
+
+
+}

5d09ae201133e5bb101c12b5960a138a3ed6cef8
danPC <x2danc@gmail.com>
2020-05-15 20:57:55 -0400

    Camera Movement

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
    public float speed = 2.0f;
+   public GameObject cameraRef;
    private Rigidbody rb;
 
    private void Start()
    {
       rb = GetComponent<Rigidbody>();
+      
    }
 
    private void Update()
    {
       CheckKeysPressed();
+      CheckMouseMoved();
    }
 
    private void CheckKeysPressed()
@@ -25,6 +28,39 @@ public class PlayerControls : MonoBehaviour
 
       }
 
+      
+
+
+
+   }
+
+   private void CheckMouseMoved()
+   {
+      if (Input.GetAxis("Mouse X") < 0) //Left
+      {
+         transform.Rotate(Vector3.down);
+      }
+
+      if (Input.GetAxis("Mouse X") > 0) //Right
+      {
+         transform.Rotate(Vector3.up);
+      }
+
+      if (Input.GetAxis("Mouse Y") < 0) //Down
+      {
+         cameraRef.transform.Rotate(Vector3.right);
+         
+      }
+
+      if (Input.GetAxis("Mouse Y") > 0) //Up
+      {
+         cameraRef.transform.Rotate(Vector3.left);
+         if (cameraRef.transform.eulerAngles.x < -60)
+         {
+            Debug.Log("FUCK");
+            cameraRef.transform.rotation = new Quaternion(-60, cameraRef.transform.rotation.y, cameraRef.transform.rotation.z, cameraRef.transform.rotation.w);
+         }
+      }
    }
 
 
